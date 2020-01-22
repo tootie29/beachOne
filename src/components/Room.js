dda8f6a8af073dc5aa281d8fe90448039f591739
tootie29 <chardmedina@gmail.com>
2020-01-22 10:59:17 +0800

    5th commit

--- src/components/Room.js
+++ src/components/Room.js
@@ -13,7 +13,7 @@ export default function Room({room}) {
                     <h6>${price}</h6>
                     <p>per night</p>
                 </div>
-                <Link to={`/rooms/${slug}`} className="btn-primary room-link">Features</Link>
+                <Link to={`/beachOne/rooms/${slug}`} className="btn-primary room-link">Features</Link>
             </div>
             <p className="room-info">{name}</p>
         </article>
